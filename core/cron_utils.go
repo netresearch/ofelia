22591f5b1e7d5a98d819b3fac6e530ad9700853c
Sebastian Mendel <info@sebastianmendel.de>
2025-05-21 13:59:00 +0200

    Forward cron key-value pairs

--- core/cron_utils.go
+++ core/cron_utils.go
@@ -1,5 +1,7 @@
 package core
 
+import "strings"
+
 // Implement the cron logger interface
 type CronUtils struct {
 	Logger Logger
@@ -10,9 +12,30 @@ func NewCronUtils(l Logger) *CronUtils {
 }
 
 func (c *CronUtils) Info(msg string, keysAndValues ...interface{}) {
-	c.Logger.Debugf(msg) // TODO, pass in the keysAndValues
+	format := cronFormatString(len(keysAndValues))
+	args := append([]interface{}{msg}, keysAndValues...)
+	c.Logger.Debugf(format, args...)
 }
 
 func (c *CronUtils) Error(err error, msg string, keysAndValues ...interface{}) {
-	c.Logger.Errorf("msg: %v, error: %v", msg, err) // TODO, pass in the keysAndValues
+	format := cronFormatString(len(keysAndValues) + 2)
+	args := append([]interface{}{msg, "error", err}, keysAndValues...)
+	c.Logger.Errorf(format, args...)
+}
+
+// cronFormatString returns a logfmt-like format string for the number of
+// key/value pairs. This mirrors the format used by robfig/cron.
+func cronFormatString(numKeysAndValues int) string {
+	var sb strings.Builder
+	sb.WriteString("%s")
+	if numKeysAndValues > 0 {
+		sb.WriteString(", ")
+	}
+	for i := 0; i < numKeysAndValues/2; i++ {
+		if i > 0 {
+			sb.WriteString(", ")
+		}
+		sb.WriteString("%v=%v")
+	}
+	return sb.String()
 }
