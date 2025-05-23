33a13a397cdaeda69757ab4972ed7d5b521a6642
Sebastian Mendel <info@sebastianmendel.de>
2025-05-23 12:22:55 +0200

    feat: preserve caller in logrus adapter

--- core/logrus_logger.go
+++ core/logrus_logger.go
@@ -1,6 +1,10 @@
 package core
 
-import "github.com/sirupsen/logrus"
+import (
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
 
 // LogrusAdapter wraps a logrus.Logger to satisfy the Logger interface.
 type LogrusAdapter struct {
@@ -9,22 +13,37 @@ type LogrusAdapter struct {
 
 var _ Logger = (*LogrusAdapter)(nil)
 
+func (l *LogrusAdapter) logf(level logrus.Level, format string, args ...interface{}) {
+	var frame *runtime.Frame
+	if pc, file, line, ok := runtime.Caller(2); ok {
+		frame = &runtime.Frame{PC: pc, File: file, Line: line, Function: runtime.FuncForPC(pc).Name()}
+	}
+
+	prev := l.Logger.ReportCaller
+	l.Logger.ReportCaller = false
+	defer func() { l.Logger.ReportCaller = prev }()
+
+	entry := logrus.NewEntry(l.Logger)
+	entry.Caller = frame
+	entry.Logf(level, format, args...)
+}
+
 func (l *LogrusAdapter) Criticalf(format string, args ...interface{}) {
-	l.Logger.Logf(logrus.FatalLevel, format, args...)
+	l.logf(logrus.FatalLevel, format, args...)
 }
 
 func (l *LogrusAdapter) Debugf(format string, args ...interface{}) {
-	l.Logger.Debugf(format, args...)
+	l.logf(logrus.DebugLevel, format, args...)
 }
 
 func (l *LogrusAdapter) Errorf(format string, args ...interface{}) {
-	l.Logger.Errorf(format, args...)
+	l.logf(logrus.ErrorLevel, format, args...)
 }
 
 func (l *LogrusAdapter) Noticef(format string, args ...interface{}) {
-	l.Logger.Infof(format, args...)
+	l.logf(logrus.InfoLevel, format, args...)
 }
 
 func (l *LogrusAdapter) Warningf(format string, args ...interface{}) {
-	l.Logger.Warnf(format, args...)
+	l.logf(logrus.WarnLevel, format, args...)
 }
