8714fd522667ae16d8be05d3c8f57cc4269cc3aa
Sebastian Mendel <info@sebastianmendel.de>
2025-05-23 07:07:33 +0200

    test: warn when daemon uses missing config

--- cli/daemon_boot_test.go
+++ cli/daemon_boot_test.go
@@ -83,3 +83,31 @@ func (s *DaemonBootSuite) TestBootLogsConfigErrorSuppressed(c *C) {
 	}
 	c.Assert(debugMsg, Equals, false)
 }
+
+func (s *DaemonBootSuite) TestBootLogsMissingConfig(c *C) {
+	tmpFile, err := os.CreateTemp("", "ofelia_missing_*.ini")
+	c.Assert(err, IsNil)
+	path := tmpFile.Name()
+	tmpFile.Close()
+	os.Remove(path)
+
+	backend, logger := newMemoryLogger(logging.DEBUG)
+	defer logging.Reset()
+	cmd := &DaemonCommand{ConfigFile: path, Logger: logger, LogLevel: "DEBUG"}
+
+	orig := newDockerHandler
+	defer func() { newDockerHandler = orig }()
+	newDockerHandler = func(notifier dockerLabelsUpdate, logger core.Logger, cfg *DockerConfig, cli dockerClient) (*DockerHandler, error) {
+		return nil, errors.New("docker unavailable")
+	}
+
+	_ = cmd.boot()
+
+	var warnMsg bool
+	for n := backend.Head(); n != nil; n = n.Next() {
+		if n.Record.Level == logging.WARNING && strings.Contains(n.Record.Message(), "Could not load config file") {
+			warnMsg = true
+		}
+	}
+	c.Assert(warnMsg, Equals, true)
+}
