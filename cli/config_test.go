e81a2d15f71341d04c28e5296915d0a88d95a816
Sebastian Mendel <info@sebastianmendel.de>
2025-06-11 22:43:33 +0200

    test: factor job source helper

--- cli/config_test.go
+++ cli/config_test.go
@@ -170,21 +170,18 @@ func (s *SuiteConfig) TestConfigIni(c *C) {
 
 		// Copy the expected job maps
 		for name, job := range t.ExpectedConfig.ExecJobs {
-			job.JobSource = JobSourceINI
 			expectedWithDefaults.ExecJobs[name] = job
 		}
 		for name, job := range t.ExpectedConfig.RunJobs {
-			job.JobSource = JobSourceINI
 			expectedWithDefaults.RunJobs[name] = job
 		}
 		for name, job := range t.ExpectedConfig.ServiceJobs {
-			job.JobSource = JobSourceINI
 			expectedWithDefaults.ServiceJobs[name] = job
 		}
 		for name, job := range t.ExpectedConfig.LocalJobs {
-			job.JobSource = JobSourceINI
 			expectedWithDefaults.LocalJobs[name] = job
 		}
+		setJobSource(expectedWithDefaults, JobSourceINI)
 
 		if !c.Check(conf, DeepEquals, expectedWithDefaults) {
 			c.Errorf("Test %q\nExpected %s, but got %s", t.Comment, toJSON(expectedWithDefaults), toJSON(conf))
@@ -477,36 +474,8 @@ func (s *SuiteConfig) TestLabelsConfig(c *C) {
 		var conf = Config{}
 		err := conf.buildFromDockerLabels(t.Labels)
 		c.Assert(err, IsNil)
-		for _, j := range conf.ExecJobs {
-			j.JobSource = JobSourceLabel
-		}
-		for _, j := range conf.RunJobs {
-			j.JobSource = JobSourceLabel
-		}
-		for _, j := range conf.ServiceJobs {
-			j.JobSource = JobSourceLabel
-		}
-		for _, j := range conf.LocalJobs {
-			j.JobSource = JobSourceLabel
-		}
-		for _, j := range conf.ComposeJobs {
-			j.JobSource = JobSourceLabel
-		}
-		for _, j := range t.ExpectedConfig.ExecJobs {
-			j.JobSource = JobSourceLabel
-		}
-		for _, j := range t.ExpectedConfig.RunJobs {
-			j.JobSource = JobSourceLabel
-		}
-		for _, j := range t.ExpectedConfig.ServiceJobs {
-			j.JobSource = JobSourceLabel
-		}
-		for _, j := range t.ExpectedConfig.LocalJobs {
-			j.JobSource = JobSourceLabel
-		}
-		for _, j := range t.ExpectedConfig.ComposeJobs {
-			j.JobSource = JobSourceLabel
-		}
+		setJobSource(&conf, JobSourceLabel)
+		setJobSource(&t.ExpectedConfig, JobSourceLabel)
 		if !c.Check(conf, DeepEquals, t.ExpectedConfig) {
 			c.Errorf("Test %q\nExpected %s, but got %s", t.Comment, toJSON(t.ExpectedConfig), toJSON(conf))
 		}
